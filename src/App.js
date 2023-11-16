37fbb0c82daf1114ee6d49e8badc36db94bd157f
MaferGpt <mariag@prettytechnical.io>
2023-11-16 16:17:12 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import racoon from "./racoon.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       {/* <img src={racoon} className="App-logo" alt="logo" />
+        <p>Racoonator</p>*/}
+      </header>
+      <body>
+        <div class="container">
+            <div class="mountain-one">
+              <div class="mountain-top">
+                <div class="mountain-cap-1"></div>
+                <div class="mountain-cap-2"></div>
+                <div class="mountain-cap-3"></div>
+              </div>
+            </div>
+            <div class="mountain-two">
+              <div class="mountain-top">
+                <div class="mountain-cap-1"></div>
+                <div class="mountain-cap-2"></div>
+                <div class="mountain-cap-3"></div>
+              </div>
+            </div>
+            <div class="mountain-three">
+              <div class="mountain-top">
+                <div class="mountain-cap-1"></div>
+                <div class="mountain-cap-2"></div>
+                <div class="mountain-cap-3"></div>
+              </div>
+            </div>
+            <div class="cloud"></div>
+
+            <div class="ground"></div>
+
+         
+          <div class="tree1"></div>
+         <div class="tree2"></div>
+          <div class="tree3"></div>
+          <div class="house">
+            <div class="roof1">
+              <div class="b1"></div>
+              <div class="b2"></div>
+            </div>
+            <div class="wall1">
+              <div class="w3">
+                <div class="window1">
+                  <div class="glass1"></div>
+                </div>
+              </div>
+            </div>
+            <div class="wall2">
+              <div class="light">
+                <div class="w1">
+                  <div class="window">
+                    <div class="glass"></div>
+                  </div>
+                </div>
+                <div class="w2">
+                  <div class="window">
+                    <div class="glass"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="door">
+                <div class="handle"></div>
+              </div>
+            </div>
+            <div class="wall3">
+              <div class="b3"></div>
+              <div class="b4"></div>
+              <div class="chimney">
+                <div class="top">
+                  <div class="smoke">
+                    <div class="s1"></div>
+                    <div class="s2"></div>
+                    <div class="s3"></div>
+                  </div>
+                  <div class="shne1"></div>
+                  <div class="shne2"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+            {/*<div class="snowfall"></div>
+          <div class="cover"></div>
+          <div class="bottom">
+            <div class="bt1"></div>
+            <div class="bt2"></div>
+          </div>
+          <div class="fence">
+            <div class="fn1">
+              <div class="screw"></div>
+            </div>
+            <div class="fn2">
+              <div class="screw"></div>
+            </div>
+            <div class="fn3">
+              <div class="screw"></div>
+            </div>
+            <div class="stck"></div>
+  </div>*/}
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
